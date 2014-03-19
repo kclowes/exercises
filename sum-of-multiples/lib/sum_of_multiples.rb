1e18f7067e2db6baca4d9c41fe7a058996178e6a
Keri Clowes <kerilyn.clowes@gmail.com>
2014-03-19 16:18:06 -0600

    Calculates multiples and sums them

---
+++ sum-of-multiples/lib/sum_of_multiples.rb
@@ -0,0 +1,12 @@
+class SumOfMultiples
+  def multiple(number)
+    three = number * 3
+    five = number * 5
+    return three, five
+  end
+  def sum(number)
+    three = number * 3
+    five = number * 5
+    three + five
+  end
+end
---
+++ sum-of-multiples/spec/sum_of_multiples_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require 'sum_of_multiples'
+
+describe SumOfMultiples do
+  it 'calculates multiples of 3 and 5' do
+   mult = SumOfMultiples.new
+    actual = mult.multiple(7)
+    expected = 21, 35
+    expect(actual).to eq(expected)
+  end
+  it 'it sum the multiples of 3 and 5' do
+    mult = SumOfMultiples.new
+    actual = mult.sum(7)
+    expected = 56
+    expect(actual).to eq(expected)
+  end
+end
